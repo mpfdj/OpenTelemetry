308126cd6cfc4f999ded3cb0aa76dd7aa4415b4a
TO11RC <miel.de.jaeger@ing.com>
2025-02-16 00:27:34 +0100

    Added build and run scripts

---
+++ opentelemetry-workshop-multi-app/opentelemetry-workshop/2.build-and-run-company-app.cmd
@@ -0,0 +1,2 @@
+call .\mvnw.cmd -f company-app/pom.xml clean package
+call .\mvnw.cmd -f company-app/pom.xml quarkus:dev
---
+++ opentelemetry-workshop-multi-app/opentelemetry-workshop/3.build-and-run-project-app.cmd
@@ -0,0 +1,2 @@
+call .\mvnw.cmd -f project-app/pom.xml clean package
+call .\mvnw.cmd -f project-app/pom.xml spring-boot:run
